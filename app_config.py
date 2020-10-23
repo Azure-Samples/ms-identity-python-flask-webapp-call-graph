59f5e1eafc031b79d9872a0e22ac1f0613981e69
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-10-23 06:36:18 -0700

    app config + app title

--- app_config.py
+++ app_config.py
@@ -1,6 +1,8 @@
-import os
 ### YOUR APP CONFIGS ###
-# this is required for encrypting flask session cookies:
-SECRET_KEY = os.environ.get('SAMPLE_APP_ENCRYPTION_KEY','enter-a-great-key') # should be loaded from a key vault or other secure location.
 # write to filesystem:
 SESSION_TYPE = 'filesystem'
+
+
+
+# sample title in navbar
+SAMPLE_DESCRIPTION = 'Authorization: Use MSAL Python to obtain an access token to call MS Graph'
